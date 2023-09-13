51acc7210192a1f5cff0df6773882859bdd13be4
Chirag <99155573+chirag011-prog@users.noreply.github.com>
2023-09-13 22:42:44 +0530

    Create MigratoryBirds.py
    Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently sighted type. If more than 1 type has been spotted that maximum amount, return the smallest of their ids.

---
+++ MigratoryBirds.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    a=0
+    s=list(set(arr))
+    for i in s:
+        c=arr.count(i)
+        if c>a:
+            a=c
+            b=i
+    return b
+    
+            
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
